65769beff8af5eac6bfa4f243be860a22addb221
AND_Programis <romanov201573@gmail.ru>
2021-10-19 22:06:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+
+            Console.WriteLine("Сколько у Вас денег?");
+            x = Convert.ToDouble(Console.ReadLine()); // считываем с консоли
+
+            // условие для какждой ситуации
+            if (x <= 50)
+            {
+                Console.WriteLine("Вы взяли мало денег");
+            } else if(x >= 250)
+            {
+                Console.WriteLine("Вы будете обедать?");
+            }
+            else if (x >= 220)
+            {
+                Console.WriteLine("Вам хватит на завтра!");
+            }
+
+            Console.ReadLine(); // задерживаем консоль 
+        }
+
+
+    }
+}
